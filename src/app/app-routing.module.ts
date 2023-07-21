ff38434c06c8b64768c37bc0271ef6329a8e69c2
SASITH <saisasith@gmail.com>
2023-07-21 14:29:51 +0530

    initial commit

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,38 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { AnalysisComponent } from './analysis/analysis.component';
+import { FileUploadComponent } from './file-upload/file-upload.component';
+import { LoginComponent } from './login/login.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
+import { RegisterComponent } from './register/register.component';
+import { HomeComponent } from './home/home.component';
+
+const routes: Routes = [
+  {
+    path: "" , redirectTo : "home" , pathMatch : "full"
+  },
+  {
+    path: "home" , component : HomeComponent
+  },
+  {
+    path: "login" , component : LoginComponent
+  },
+  {
+    path: "register", component: RegisterComponent
+  },
+  {
+    path: "file-upload", component: FileUploadComponent
+  },
+  {
+    path: "analysis" , component: AnalysisComponent
+  },
+    { 
+      path: '**', pathMatch: 'full', component: PageNotFoundComponent 
+    }
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
---
+++ src/app/app.module.ts
@@ -0,0 +1,82 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { RouterModule, Routes } from '@angular/router';
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component';
+import { MatCardModule } from '@angular/material/card';
+import { MatInputModule } from '@angular/material/input';
+import { ReactiveFormsModule } from '@angular/forms';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { FormsModule } from '@angular/forms';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatButtonModule } from '@angular/material/button';
+import { MatSliderModule } from '@angular/material/slider';
+import { HttpClientModule } from "@angular/common/http";
+import { FileUploadComponent } from './file-upload/file-upload.component';
+import { MatIconModule } from '@angular/material/icon';
+import { AnalysisComponent } from './analysis/analysis.component';
+import { Angular2CsvModule } from 'angular2-csv';
+import { MatSelectModule } from '@angular/material/select';
+import { MatButtonToggleModule } from '@angular/material/button-toggle';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
+import { HomeComponent } from './home/home.component';
+
+
+const routes: Routes = [
+  {
+    path: "" , redirectTo : "home" , pathMatch : "full"
+  },
+  {
+    path: "home" , component : HomeComponent
+  },
+  {
+    path: "login" , component : LoginComponent
+  },
+  {
+    path: "register", component: RegisterComponent
+  },
+  {
+    path: "file-upload", component: FileUploadComponent
+  },
+  {
+    path: "analysis" , component: AnalysisComponent
+  },
+   { 
+      path: '**', pathMatch: 'full', component: PageNotFoundComponent
+    }
+];
+@NgModule({
+  declarations: [
+    AppComponent,
+    LoginComponent,
+    RegisterComponent,
+    FileUploadComponent,
+    AnalysisComponent,
+    PageNotFoundComponent,
+    HomeComponent,
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    MatCardModule,
+    MatInputModule,
+    ReactiveFormsModule,
+    MatFormFieldModule,
+    FormsModule,  
+    BrowserAnimationsModule,
+    MatButtonModule,
+    MatSliderModule,
+    HttpClientModule,
+    MatIconModule,
+    Angular2CsvModule,
+    MatSelectModule,
+    MatButtonToggleModule,
+    RouterModule.forRoot(routes)
+    
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
